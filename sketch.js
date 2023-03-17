c5af480aaf9b2b77b4cb59f54b4cb3f22e81b978
ileini-melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-17 08:55:59 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let dot1;
+
+function setup() {
+  createCanvas(windowWidth - 20, windowHeight - 20);
+  colorMode(HSL, 360, 100, 100);
+  dot1 = new BouncyDot();
+}
+
+function draw() {
+  background(220, 0, 80);
+  dot1.float();
+  dot1.display();
+}
+
+class BouncyDot {
+  constructor() {
+    // Randomly generate position
+    this.x = random(width);
+    this.y = random(height);
+    // Randomly generate radius
+    this.r = random(5, 12);
+    // Randomly generate color
+    this.color = random(360);
+    // Randomly generate a standard velocity (broken into components)...
+    this.standardXvelocity = random(0.5, 3);
+    this.standardYvelocity = random(0.5, 3);
+    // ...and use those as starting velocities.
+    this.xVelocity = this.standardXvelocity;
+    this.yVelocity = this.standardYvelocity;
+  }
+
+  float() {
+    this.x += this.xVelocity;
+    this.y += this.yVelocity;
+    // Standard bounce code - like the DVD logo, but for spheres.
+    if (this.x + this.r > width) {
+      this.xVelocity = -1 * this.standardXvelocity;
+    }
+    if (this.x - this.r < 0) {
+      this.xVelocity = this.standardXvelocity;
+    }
+    if (this.y + this.r > height) {
+      this.yVelocity = -1 * this.standardYvelocity;
+    }
+    if (this.y - this.r < 0) {
+      this.yVelocity = this.standardYvelocity;
+    }
+  }
+
+  display() {
+    fill(this.color, 80, 70);
+    noStroke();
+    ellipse(this.x, this.y, this.r * 2);
+  }
+}
